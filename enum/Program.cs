c6ddca253296c76f2a9d0d913c172ff16bb635e3
sirivlucifer <58220908+sirivlucifer@users.noreply.github.com>
2022-03-22 22:01:22 +0300

    main

---
+++ enum/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Pazar);
+enum Gunler{
+
+    Pazartesi,
+    Salı,
+    Çarşamba,
+    Perşembe=45;
+    Cuma,
+    Cumartesi,
+    Pazar
+}
